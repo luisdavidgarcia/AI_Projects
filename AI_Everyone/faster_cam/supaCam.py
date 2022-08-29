1d8074fff6d40e30b8eb78152cad3c57bbedd0f9
Luis David Garcia <hookeri_bartz@aleeas.com>
2022-08-29 13:57:00 -0700

    Made function to manage multiple windows

--- AI_Everyone/faster_cam/supaCam.py
+++ AI_Everyone/faster_cam/supaCam.py
@@ -2,16 +2,27 @@ import cv2
 import imutils
 
 print(cv2.__version__)
-width=640
-height=360
+width=1280
+height=700
+rows=3
+columns=4
 cam=cv2.VideoCapture(0)
 
 while True:
     ignore, frame =  cam.read()
 
-    frame = imutils.resize(frame, width, height)
-    cv2.imshow('myCam', frame)
-    cv2.moveWindow('myCam',0,0)
+    #frame = imutils.resize(frame, width, height)
+    frame = cv2.resize(frame,(int(width/columns), int(height/columns)))
+
+    for i in range(0, rows):
+        for j in range(0, columns):
+            window_name = f"Window_{i}x{j}"
+            scaled_width = j * int(width/columns)
+            scaled_height = i * int(height/columns + 30)           
+            cv2.imshow(window_name, frame)
+            cv2.moveWindow(window_name, scaled_width, scaled_height)
+
+
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
